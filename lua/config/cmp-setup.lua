f0ea75b2761acfb2c57c49cf140e8f93a3773704
jovryy <joelvolkwein@googlemail.com>
2025-11-12 22:11:36 +0100

    cmp: removed 2 incorrect language servers

--- lua/config/cmp-setup.lua
+++ lua/config/cmp-setup.lua
@@ -35,10 +35,8 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('mason-lspconfig').setup({
 		ensure_installed = { 
 		'lua_ls',
-		'tsserver',
 		'clangd', 
 		'pyright', 
-		'volar', 
 		'cssls', 
 		'html', 
 		'jsonls',
